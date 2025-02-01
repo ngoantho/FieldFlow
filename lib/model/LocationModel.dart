5e819eb402667d27d5477739fb1f403565d88536
quangtran0603 <qtran1@seattleu.edu>
2025-02-01 15:15:52 -0800

    Create LocationModel.dart
    Create LocationModel (Contain Latitude, Longitude and Duration at that location)

---
+++ lib/model/LocationModel.dart
@@ -0,0 +1,7 @@
+class LocationModel{
+  double latitude; //North or South
+  double longitude; //West or East
+  double duration; //time spent at a certain location
+
+  LocationModel(this. latitude, this.longitude, this.duration);
+}

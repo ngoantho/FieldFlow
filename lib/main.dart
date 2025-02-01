342969a5e996621dec164946c5acc004101e7301
Anthony Ngo <anthony.ngo.dev@gmail.com>
2025-02-01 14:55:32 -0800

    test commit

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Testing Git'),
+      home:
+          const MyHomePage(title: 'Flutter Demo Home Testing Git from Anthony'),
     );
   }
 }

4a84550bb0fa130e5dcfdff31372299141328ade
quangtran0603 <qtran1@seattleu.edu>
2025-02-02 13:01:08 -0800

    Initiate and Design the Day Detail Page

---
+++ lib/day_detail.dart
@@ -0,0 +1,65 @@
+import 'package:field_flow/model/day_model.dart';
+import 'package:flutter/material.dart';
+
+class DayDetail extends StatelessWidget {
+  final DayModel dayModel;
+  const DayDetail({super.key, required this.dayModel});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+        child: Column(
+      children: [
+        Padding(
+            padding: EdgeInsets.all(8),
+            child: Text(
+                "${dayModel.dayOfWeek} - ${dayModel.getDdMmYyyy} - Detail Report \n"
+                "Check-in at: ${dayModel.checkEntry.checkInTime} \n"
+                "Check-out at: ${dayModel.checkEntry.checkOutTime}\n\n\n")),
+        ...dayModel.locationList.map((location) {
+          return Row(
+            children: [
+              Expanded(
+                child: Text(
+                    '           You spent ${location.duration.inHours} hours at Location: ${location.latitude} - ${location.longitude}\n\n\n',
+                  textAlign: TextAlign.justify
+                )
+              ),
+            ],
+          );
+        }),
+      ],
+    ));
+    // return SingleChildScrollView(
+    //     child: Column(
+    //       crossAxisAlignment: CrossAxisAlignment                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             .start,
+    //       children:[
+    //         Padding(
+    //           padding: EdgeInsets.all(8.0),
+    //           child: Text(
+    //               "${dayModel.dayOfWeek} - ${dayModel.getDdMmYyyy} - Detail Report\n"
+    //                                   "Check-in at: ${dayModel.checkEntry.checkInTime}\n"
+    //                                   "Check-out at: ${dayModel.checkEntry.checkOutTime}"
+    //           ),
+    //         ),
+    //         //Workout Exercise List out
+    //         ...dayModel.locationList.map((location) {
+    //           return Row(
+    //             children: [
+    //               Expanded(
+    //                 child: Text(
+    //                     'You spent ${location.duration.inHours} hours at Location: ${location.latitude} - ${location.longitude} \n'
+    //                 ),
+    //               ),
+    //
+    //             ],
+    //           );
+    //         }).toList(),
+    //
+    //       ],
+    //
+    //
+    //     )
+    // );
+  }
+}
---
+++ lib/day_detail_page.dart
@@ -0,0 +1,17 @@
+import 'package:field_flow/model/day_model.dart';
+import 'package:flutter/material.dart';
+
+import 'day_detail.dart';
+
+class DayDetailPage extends StatelessWidget {
+  final DayModel dayModel;
+  const DayDetailPage({super.key, required this.dayModel});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: DayDetail(dayModel: dayModel,),
+    );
+  }
+}

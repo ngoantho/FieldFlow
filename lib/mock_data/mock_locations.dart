d42ef4ad2dd8de976aa1a7b32b65cc31c4de94cf
Anthony Ngo <anthony.ngo.dev@gmail.com>
2025-02-02 14:07:14 -0800

    add time tracking provider

---
+++ lib/mock_data/mock_locations.dart
@@ -0,0 +1,7 @@
+import '../model/location_model.dart';
+
+final mockLocations = [
+  LocationModel(47.6205, -122.3493, Duration(hours: 3)),
+  LocationModel(47.6097, -122.3410, Duration(hours: 3)),
+  LocationModel(47.6094, -122.3180, Duration(hours: 2))
+];
---
+++ lib/time_tracker_provider.dart
@@ -0,0 +1,63 @@
+import 'package:field_flow/mock_data/mock_locations.dart';
+import 'package:field_flow/model/check_entry_model.dart';
+import 'package:field_flow/model/day_model.dart';
+import 'package:field_flow/model/week_model.dart';
+import 'package:flutter/material.dart';
+
+class TimeTracker with ChangeNotifier {
+  DateTime? checkInTime;
+  DateTime? checkOutTime;
+  List<WeekModel> weekList = [];
+  WeekModel? currentWeek;
+  bool? checkedIn;
+
+  void checkIn() {
+    checkedIn = true;
+    checkInTime = DateTime.now();
+  }
+
+  void checkOut() {
+    checkedIn = false;
+    checkOutTime = DateTime.now();
+
+    if (checkInTime == null || checkOutTime == null) return;
+
+    // fill in times
+    final CheckEntryModel checkEntry = CheckEntryModel(checkInTime, checkOutTime);
+
+    // create day model
+    final newDay = DayModel(checkEntry, mockLocations);
+
+    // add day to week
+    addDayToWeek(newDay);
+
+    checkInTime = null;
+    checkOutTime = null;
+
+    notifyListeners();
+  }
+
+  addDayToWeek(DayModel day) {
+    DateTime checkInDate = day.checkEntry.checkInTime!;
+
+    /*
+      .weekday returns a value from 1 to 7
+      .weekday - 1 gets how many days have passed since monday
+      subtract .weekday - 1 moves the date back to Monday of that week
+     */
+    DateTime startWeekFlag = checkInDate.subtract(Duration(days: checkInDate.weekday - 1));
+
+    DateTime endWeekFlag = startWeekFlag.add(Duration(days: 6, hours: 23, minutes: 59, seconds: 59));
+
+    if (currentWeek == null || currentWeek!.dayList.isEmpty || checkInDate.isAfter(endWeekFlag)) {
+      // new week
+      if (currentWeek != null && currentWeek!.dayList.isNotEmpty) {
+        weekList.add(currentWeek!);
+      }
+      currentWeek = WeekModel([day]);
+    } else {
+      // add to existing week
+      currentWeek!.dayList.add(day);
+    }
+  }
+}

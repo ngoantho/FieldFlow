99dba80229cb4a4d05b2731f788707d74471719e
Anthony Ngo <anthony.ngo.dev@gmail.com>
2025-02-04 17:32:22 -0800

    Add coverage script

---
+++ test/full_coverage_test.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: unused_import
+import 'package:field_flow/banner.dart';
+import 'package:field_flow/day_detail/day_detail.dart';
+import 'package:field_flow/day_detail/day_detail_page.dart';
+import 'package:field_flow/day_list/day_list.dart';
+import 'package:field_flow/day_list/day_list_history_page.dart';
+import 'package:field_flow/homepage.dart';
+import 'package:field_flow/main.dart';
+import 'package:field_flow/mixins/build_app_bar.dart';
+import 'package:field_flow/mixins/dialog_confirm.dart';
+import 'package:field_flow/mixins/navigate_mixin.dart';
+import 'package:field_flow/mock_data/mock_data.dart';
+import 'package:field_flow/mock_data/mock_locations.dart';
+import 'package:field_flow/model/check_entry_model.dart';
+import 'package:field_flow/model/day_model.dart';
+import 'package:field_flow/model/location_model.dart';
+import 'package:field_flow/model/week_model.dart';
+import 'package:field_flow/nav_menu.dart';
+import 'package:field_flow/providers/position_provider.dart';
+import 'package:field_flow/providers/time_tracker.dart';
+import 'package:field_flow/week_list/week_list.dart';
+import 'package:field_flow/week_list/week_list_history_page.dart';
+
+void main() {}

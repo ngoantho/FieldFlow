e8c69d570ae2a1f81a6bcc05f2d061d04f9fcfd9
quangtran0603 <qtran1@seattleu.edu>
2025-02-07 19:01:43 -0800

    Change test name
    Change test name to be more understandable

--- test/check_entry_test.dart
+++ test/check_entry_test.dart
@@ -2,22 +2,22 @@ import 'package:field_flow/model/check_entry_model.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  group('Check in/out', () {
-    test('check in', () {
+  group('CheckEntryModel Test Group', () {
+    test('Test if Check In successfully captured now() time', () {
       final model = CheckEntryModel();
       expect(model.checkInTime, isNull);
 
       model.checkIn();
       expect(model.checkInTime, equals(DateTime.now()));
     });
-    test('check out', () {
+    test('Test Check Out successfully captured now() time', () {
       final model = CheckEntryModel();
       expect(model.checkOutTime, isNull);
 
       model.checkOut();
       expect(model.checkOutTime, equals(DateTime.now()));
     });
-    test('reset', () {
+    test('Test if model successful reset its variable after called reset()', () {
       final model = CheckEntryModel();
       model.checkInTime = DateTime.now();
       model.checkOutTime = DateTime.now();
@@ -28,7 +28,7 @@ void main() {
     });
   });
   group('format datetime as HH:mm:ss', () {
-    test('DateTime extension', () {
+    test('Test if getHHmmss() returns expected time format as HH:mm:ss', () {
       final testDate = DateTime.now();
       final timeFormat = RegExp(r'^\d{2}:\d{2}:\d{2}$');
       expect(testDate.getHHmmss(), matches(timeFormat));

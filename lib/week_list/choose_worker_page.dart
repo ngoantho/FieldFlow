8d48f58341eb0258b2f5ef39beef5553cfbbc4b2
Anthony Ngo <anthony.ngo.dev@gmail.com>
2025-02-23 15:50:29 -0800

    manager page: mock list of 7 workers
    Co-Authored-By: quangtran0603 <146675582+quangtran0603@users.noreply.github.com>

--- lib/week_list/choose_worker_page.dart
+++ lib/week_list/choose_worker_page.dart
@@ -1,15 +1,41 @@
+import 'package:field_flow/week_list/week_list.dart';
 import 'package:flutter/material.dart';
 
-class ChooseWorkerPage extends StatefulWidget {
+class ChooseWorkerPage extends StatelessWidget {
   const ChooseWorkerPage({super.key});
 
   @override
-  State<ChooseWorkerPage> createState() => _ChooseWorkerPageState();
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (context, index) => Padding(
+        padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+        child: _ChooseWorkerItem(index + 1),
+      ),
+      itemCount: 7,
+    );
+  }
 }
 
-class _ChooseWorkerPageState extends State<ChooseWorkerPage> {
+class _ChooseWorkerItem extends StatelessWidget {
+  final int _index;
+
+  const _ChooseWorkerItem(this._index, {super.key});
+
+  String get index => _index.toString();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ElevatedButton(
+        onPressed: () {
+          Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+            return Scaffold(
+                appBar: AppBar(
+                  title: Text('Worker (ID: $index)'),
+                  centerTitle: true,
+                ),
+                body: WeekList(id: index));
+          }));
+        },
+        child: Text(index));
   }
 }

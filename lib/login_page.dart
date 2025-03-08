648f55763eb1bcabf8ba4ef99c422c2ede3ee8d4
quangtran0603 <qtran1@seattleu.edu>
2025-03-08 12:38:06 -0800

    Log in using google

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,10 +1,35 @@
+import 'package:field_flow/providers/auth_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final authProvider = Provider.of<AuthProvider>(context, listen: false);
+
+    return Scaffold(
+      appBar: AppBar(title: Text('Login')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Welcome to FieldFlow!', style: TextStyle(fontSize: 20)),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () async {
+                final user = await authProvider.signInWithGoogle();
+                if (user != null) {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => UserRoleSelectionPage(user: user)),
+                  );
+                }
+              },
+              child: Text('Sign in with Google'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }

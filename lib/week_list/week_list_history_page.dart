e6285debb652fcb789bc003a105f0f0a62b38912
quangtran0603 <qtran1@seattleu.edu>
2025-03-08 13:08:31 -0800

    Update week_list_history_page.dart

--- lib/week_list/week_list_history_page.dart
+++ lib/week_list/week_list_history_page.dart
@@ -1,27 +1,32 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:field_flow/mixins/build_app_bar.dart';
 import 'package:field_flow/week_list/choose_worker_page.dart';
 import 'package:field_flow/week_list/week_list.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class WeekListHistoryPage extends StatelessWidget with BuildAppBar {
+
   const WeekListHistoryPage({super.key});
 
+  Future<String> _getUserRole() async {
+    User? user = FirebaseAuth.instance.currentUser;
+
+    DocumentSnapshot userDoc = await FirebaseFirestore.instance.collection('users').doc(user!.uid).get();
+    return userDoc['role'];
+  }
+  
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          appBar: buildAppBar(title: 'Week Report'),
-          body: TabBarView(children: [
-            WeekList(id: DateTime.now().weekday.toString()),
-            ChooseWorkerPage()
-          ]),
-          bottomNavigationBar: TabBar(tabs: [
-            Tab(text: 'Worker (ID: ${DateTime.now().weekday.toString()})'),
-            Tab(
-              text: 'Manager',
-            )
-          ]),
-        ));
+    return FutureBuilder<String>
+      (future: _getUserRole(),
+        builder: (context, snapshot){
+          return Scaffold(
+            appBar: AppBar(title: Text('Week Report')),
+            body: snapshot.data == 'Manager'
+                ? ChooseWorkerPage()   // Manager sees worker selection
+                : WeekList(id: FirebaseAuth.instance.currentUser?.uid ?? ''), // Field Worker sees their week list
+          );
+        });
   }
 }

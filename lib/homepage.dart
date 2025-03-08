cf808a5c8daf314f9b61f42f5eeea51d47844b63
Anthony Ngo <anthony.ngo.dev@gmail.com>
2025-03-08 14:14:08 -0800

    homepage: add role

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,7 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:field_flow/mixins/build_app_bar.dart';
 import 'package:field_flow/mixins/dialog_confirm.dart';
 import 'package:field_flow/providers/time_tracker.dart';
 import 'package:field_flow/week_list/week_list_history_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:provider/provider.dart';
@@ -37,22 +39,36 @@ class _HomepageState extends State<Homepage>
   Widget build(BuildContext context) {
     final timeTracker = context.watch<TimeTracker>();
 
-    return Scaffold(
-        appBar: buildAppBar(title: 'FieldFlow'),
-        floatingActionButton: ElevatedButton(
-            onPressed: () {
-              Navigator.of(context).push(
-                  MaterialPageRoute(builder: (_) => WeekListHistoryPage()));
-            },
-            child: Text('Week Report')),
-        body: Center(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            if (!timeTracker.checkedOut) CheckInOutButton(),
-            SizedBox(height: 10),
-            CheckedInOutText(),
-          ],
-        )));
+    return FutureBuilder(future: (() async {
+      User? user = FirebaseAuth.instance.currentUser;
+      final userDoc = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(user!.uid)
+          .get();
+      return userDoc['role'];
+    })(), builder: (context, snapshot) {
+      if (!snapshot.hasData) {
+        return Scaffold(
+            appBar: buildAppBar(title: 'FieldFlow'),
+            body: Center(child: CircularProgressIndicator()));
+      }
+      return Scaffold(
+          appBar: buildAppBar(title: 'FieldFlow', subtitle: snapshot.data),
+          floatingActionButton: ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (_) => WeekListHistoryPage()));
+              },
+              child: Text('Week Report')),
+          body: Center(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              if (!timeTracker.checkedOut) CheckInOutButton(),
+              SizedBox(height: 10),
+              CheckedInOutText(),
+            ],
+          )));
+    });
   }
 }
--- lib/mixins/build_app_bar.dart
+++ lib/mixins/build_app_bar.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
 
 mixin BuildAppBar {
-  AppBar buildAppBar({required String title}) {
-    return AppBar(title: Text(title), centerTitle: true,);
+  AppBar buildAppBar({required String title, String? subtitle}) {
+    return AppBar(
+      title: Text(title),
+      centerTitle: true,
+      bottom: subtitle != null
+          ? PreferredSize(
+              preferredSize: Size.fromHeight(20.0),
+              child: Text(subtitle, style: TextStyle(fontSize: 16)),
+            )
+          : null,
+    );
   }
 }

2b5bdad1466350abd4ff649a67fef299a3e030f5
quangtran0603 <qtran1@seattleu.edu>
2025-03-08 11:57:25 -0800

    Create auth_provider to support user sign in
    Creating and Storing user (with firebase)

---
+++ lib/providers/auth_provider.dart
@@ -0,0 +1,33 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:field_flow/db/firestore_helper.dart';
+
+class AuthProvider {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirestoreHelper _firestoreHelper;
+
+  AuthProvider({required FirestoreHelper firestoreHelper})
+      : _firestoreHelper = firestoreHelper;
+
+  Future<User?> signInWithGoogle() async {
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+    if (googleUser == null) return null;
+
+    final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+    final AuthCredential credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    final UserCredential userCredential = await _auth.signInWithCredential(credential);
+    final User? user = userCredential.user;
+
+    if (user != null) {
+      final userDoc = await _firestoreHelper.getUser(user.uid);
+      if (!userDoc.exists) {
+        await _firestoreHelper.createUser(user.uid, user.displayName, user.email);
+      }
+    }
+    return user;
+  }
+}

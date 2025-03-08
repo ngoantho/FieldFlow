93dc2287f3ab81c591371ac638da09c5bc261aae
Anthony Ngo <anthony.ngo.dev@gmail.com>
2025-03-08 12:31:39 -0800

    auth gate pages

---
+++ lib/auth_gate.dart
@@ -0,0 +1,45 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:field_flow/homepage.dart';
+import 'package:field_flow/role_selection.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import 'login_page.dart';
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return CircularProgressIndicator();
+          }
+
+          final user = snapshot.data;
+          if (user == null) {
+            return LoginPage();
+          }
+
+          return FutureBuilder(
+              future: FirebaseFirestore.instance
+                  .collection('users')
+                  .doc(user.uid)
+                  .get(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return CircularProgressIndicator();
+                }
+
+                final userDoc = snapshot.data!;
+                if (!userDoc.exists || userDoc['role'] == null) {
+                  return UserRoleSelectionPage(user: user);
+                }
+
+                return Homepage();
+              });
+        });
+  }
+}
---
+++ lib/role_selection.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class UserRoleSelectionPage extends StatelessWidget {
+  const UserRoleSelectionPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

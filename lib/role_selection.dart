640772365e43e5e5d5f2b198c07d08b6b98fc2ad
Anthony Ngo <anthony.ngo.dev@gmail.com>
2025-03-08 12:36:56 -0800

    role selection: pass in user

--- lib/role_selection.dart
+++ lib/role_selection.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class UserRoleSelectionPage extends StatelessWidget {
-  const UserRoleSelectionPage({super.key});
+  final User user;
+
+  const UserRoleSelectionPage({super.key, required this.user});
 
   @override
   Widget build(BuildContext context) {

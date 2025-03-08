4103baa13e6adbbc7c8d81e96cd83d57c8919f47
Anthony Ngo <anthony.ngo.dev@gmail.com>
2025-03-08 13:21:11 -0800

    Update auth_provider.dart

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -1,6 +1,6 @@
+import 'package:field_flow/db/firestore_helper.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:field_flow/db/firestore_helper.dart';
 
 class AuthProvider {
   final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -10,22 +10,28 @@ class AuthProvider {
       : _firestoreHelper = firestoreHelper;
 
   Future<User?> signInWithGoogle() async {
-    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+    final GoogleSignInAccount? googleUser = await GoogleSignIn(
+            serverClientId:
+                "995776545775-3uiro9i0ur67si8onnfagjbohr4lf9tr.apps.googleusercontent.com")
+        .signIn();
     if (googleUser == null) return null;
 
-    final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser.authentication;
     final AuthCredential credential = GoogleAuthProvider.credential(
       accessToken: googleAuth.accessToken,
       idToken: googleAuth.idToken,
     );
 
-    final UserCredential userCredential = await _auth.signInWithCredential(credential);
+    final UserCredential userCredential =
+        await _auth.signInWithCredential(credential);
     final User? user = userCredential.user;
 
     if (user != null) {
       final userDoc = await _firestoreHelper.getUser(user.uid);
       if (!userDoc.exists) {
-        await _firestoreHelper.createUser(user.uid, user.displayName, user.email);
+        await _firestoreHelper.createUser(
+            user.uid, user.displayName, user.email);
       }
     }
     return user;
